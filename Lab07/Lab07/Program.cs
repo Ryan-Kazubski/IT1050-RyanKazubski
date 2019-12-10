2feede8dcc21495611fcf8d11087fc22687c39f5
Ryan Kazubski <S01235247@acad.tri-c.edu>
2019-12-09 19:04:32 -0500

    Lab 07

---
+++ Lab07/Lab07/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Lab07
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("A Tale of Two Cities", "Charles Dickens", "1859");
+            book1.Display();
+
+            BookTest book2 = new BookTest("House of Hades", "Rick Riordan", "2013");
+            book2.Display();
+        }
+    }
+
+
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string Year;
+
+        public Book(string Title, string Author, string Year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Year = Year;
+        }
+
+        public void title(string Title)
+        {
+            this.Title = Title;
+        }
+
+        public void author(string Author)
+        {
+            this.Author = Author;
+        }
+
+        public void year(string Year)
+        {
+            this.Year = Year;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(
+                $"Title: {this.Title}\n" +
+                $"Author: {this.Author}\n" +
+                $"Year: {this.Year}\n");
+        }
+
+
+    }
+    class BookTest
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public BookTest(string Title, string Author, string Year)
+            {
+                this.Title = Title;
+                this.Author = Author;
+                this.Year = Year;
+            }
+        public void Display()
+        {
+            Console.WriteLine(
+                $"Title: {this.Title}\n" +
+                $"Author: {this.Author}\n" +
+                $"Year: {this.Year}\n");
+        }
+    }
+
+    /* 3.) We are creating an object that will go and retrieve our variables and returns from our methods and constructors.
+     * 4.)we would be practicing object orientd coding.
+     * 5.)The core advantage of exception handling is to maintain the normal flow of the application. An exception normally disrupts the normal flow of the application that is why we use exception handling.
+     * 6.) private allows you to hide things in methods and propertities while public gives you the freedom to use those anyhwere elde.
+     * 7.) It describes a class that references one or more objects of other classes in instance variables. This allows you to model a has-a association between objects. would allow our code to repeat things in other methods without retyping it.
+     * 8.) Data abstraction refers to providing only essential information to the outside world and hiding their background details, i.e., to represent the needed information in program without presenting the details.
+     */
+
+
+
+
+
+
+}
+
---
+++ Midterm/Midterm/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean keeplooping = true;
+
+            while (keeplooping == true)
+            {
+                Console.WriteLine("0");
+            }
+
+            int i = 2;
+
+            while (i < 128)
+            {
+                Console.WriteLine("[" + i + "]");
+
+                if (i == 64)
+                {
+                    break;
+                }
+                i = i * 2;
+            }
+
+            Console.WriteLine("\n");
+            for (i = 49; i >= 1; --i)
+            {
+
+                Console.Write(i);
+
+                if (i >= 2)
+                {
+                    Console.Write(",");
+                }
+
+            }
+
+            i = 1;
+
+            Console.WriteLine("\n");
+
+            while (i <= 21)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write($"{i} ");
+                }
+
+                i = i + 1;
+            }
+
+            Console.WriteLine("\n");
+
+            Boolean icyrain = false;
+            Boolean tornadowarning = false;
+
+            if (icyrain == false && tornadowarning == false)
+            {
+                Console.WriteLine("lets go outside");
+            }
+
+            Console.WriteLine("\n");
+
+            int num = 5;
+
+            for (int k = 5; k >= 1; --k)
+            {
+                for (int h = 5; h > num; h--)
+                {
+                    Console.Write(" ");
+                }
+                for (int h = 1; h <= (num - 1); ++h)
+                {
+                    Console.Write(h);
+                }
+                for (int h = 0; h < num; ++h)
+                {
+                    Console.Write(num - h);
+                }
+
+                Console.WriteLine();
+
+                num = num - 1;
+            }
+
+            /* What is the output for the following code ?
+                            What would the output be with the exact same code but using a while statement as opposed to
+                            a do -while?
+                            A : The do while loop only outputs a single “*” on the other hand the while loop doesn’t output
+                any because the value is already greater then N. The while loop will check the condition before executing the program and the do while
+                will execute the program then check the condition after
+                Explain how do we combine multiple Boolean values? 
+                Write an if statement that outputs “Let’s 
+                go outside!” when both Boolean values are false.
+ A: You can also combine true/false values by 
+                using the Boolean operators, which take true/false values as operands and compute new 
+                true/false values
+ 2. Identify and briefly describe 3 types of control structures that we can use to order the statements in our programs?
+                The three basic types of control structures are sequential, selection and iteration. They can be combined in any way to solve a specified problem.
+                Sequential is the default control structure, statements are executed line by line in the order in which they appear. The selection structure is 
+                used to test a condition. A sequence of statements is executed depending on whether or not the condition it true or false. This means the program 
+                chooses between two or more alternative paths. Condition refers to any expression or value that returns a Boolean value, meaning true or false.
+                The three main types of selection statements are "if," "if/else" and "switch" statements. The most basic and common is the "if" statement. The "if" 
+                and "if/else" statements can be nested. Switch statements are ideally used when there are multiple cases to choose from.
+                The iteration or repetition structure repeatedly executes a series of statements as long as the condition is true. The condition may be predefined 
+                or open-ended. "While," "do/while" and "for" loop are the three types of iteration statements. A loop can either be event controlled or counter 
+                controlled. An event-controlled loop executes a sequence of statements till and event occurs while a counter-controlled loop executes the statements a predetermined number of times.
+
+                */
+        }
+    }
+}
